c3c431843f7ab5bb8d1aca086751680cc12120e5
activatethefud <nikolasutic1@gmail.com>
2021-04-10 11:04:34 +0200

    Added volunteer assignment to task, and free tasks listing.

--- backend/api/routes/task.js
+++ backend/api/routes/task.js
@@ -34,7 +34,52 @@ router.post('/listTasks', async (req, res, next) => {
         res.json(allTasks);
 })
 
+router.post('/listFreeTasks', async (req,res,next) => {
+
+        const freeTasks = await Task.find({'volunteer_id' : {$exists:false}}).exec();
+
+        res.json(freeTasks);
+});
+
+//router.post('/listActiveTasks',async (req,rex,next) => {
+//});
+
+router.post('/assign/:id1/:id2',async (req,res,next) => {
+
+        // Assign volunteer with id1 to requester with id2
+
+        const jmbg1 = req.params.id1;
+        const jmbg2 = req.params.id2;
+
+        const user1 = await User.findOne({'jmbg':jmbg1}).exec();
+        const user2 = await User.findOne({'jmbg':jmbg2}).exec();
+
+        if(user1 == null || user2 == null) {
+                res.status(404).json({
+                        message: "Failed to find user!"
+                });
+        }
+
+        const task = await Task.findOne({'request_user_id' : user2._id}).exec();
+
+        if(task == null) {
+                res.status(404).json({
+                        message: "Task not found!"
+                });
+        }
+
+        await task.updateOne({"volunteer_id": user1._id}).exec().catch(function(err) {
+                next(err);
+        });
+
+        res.status(201).json({
+                message: "Added volunteer to task",
+                info: [user1,user2,task]
+        });;
+});
+
 router.post('/new', async (req, res, next) => {
+
         try{
                 const taskOwner = await User.findOne({"jmbg":req.body.jmbg}).exec();
 
@@ -43,7 +88,9 @@ router.post('/new', async (req, res, next) => {
                 const newTask = new Task({
                         _id : new mongoose.Types.ObjectId(),
                         request_user_id: taskOwner._id,
-                        task_string: req.body.task_string
+                        task_string: req.body.task_string,
+                        latitude: parseFloat(req.body.latitude),
+                        longitude: parseFloat(req.body.longitude)
                 });
 
                 await newTask.save();
